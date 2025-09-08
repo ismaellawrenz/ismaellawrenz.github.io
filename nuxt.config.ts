577b9e62c4e69ab0bb1eb836a7f638ea22d8d626
Ismael <ismaellawrenz@gmail.com>
2025-09-08 15:08:13 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+   app: {
+    head: {
+      title: 'Ismael Luan Lawrenz - Engenheiro de Sofware',       
+      htmlAttrs: {
+        lang: 'pt',
+      },
+      meta: [
+        { name: 'description', content: 'Engenheiro de software especializado em desenvolvimento com Java e GO.' },
+        { name: 'keywords', content: 'desenvolvimento, software, desenvolvedor, fron-end, back-end, Concórdia,programador, web' },
+        { name: 'author', content: 'Ismael Luan Lawrenz' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { charset: 'utf-8' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: 'Ill.ico' },
+      ]
+    }
+  }
+})
